0360ccb3f5b2bb1a4b1198c06a3d3abbf15b1def
White11010 <beliavski26@gmail.com>
2022-10-13 12:46:35 +0300

    add custom eslint rules

--- src/preload.ts
+++ src/preload.ts
@@ -1,39 +1,39 @@
-import { ipcRenderer } from "electron";
-import {EMainCustomEvents} from "./typings/custom-events";
+import { ipcRenderer } from 'electron';
+import {EMainCustomEvents} from './models/custom-events';
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener('DOMContentLoaded', () => {
   const DOMElementsMap = {
-    saveDocumentButton: document.getElementById("save-button"),
-    createDocumentButton: document.getElementById("new-button"),
-    openDocumentButton: document.getElementById("open-button"),
-    closeApplicationButton: document.getElementById("close-button"),
-    textareaInput: document.getElementById("textarea-input") as HTMLInputElement,
-    documentName: document.getElementById("document-name"),
-    documentNameStar: document.getElementById("document-name-star"),
+    saveDocumentButton: document.getElementById('save-button'),
+    createDocumentButton: document.getElementById('new-button'),
+    openDocumentButton: document.getElementById('open-button'),
+    closeApplicationButton: document.getElementById('close-button'),
+    textareaInput: document.getElementById('textarea-input') as HTMLInputElement,
+    documentName: document.getElementById('document-name'),
+    documentNameStar: document.getElementById('document-name-star'),
   };
 
   // DOM elements event listeners
-  DOMElementsMap.saveDocumentButton.addEventListener("click", () => {
-    ipcRenderer.send("save-document-triggered", {
+  DOMElementsMap.saveDocumentButton.addEventListener('click', () => {
+    ipcRenderer.send('save-document-triggered', {
       inputText: DOMElementsMap.textareaInput.value,
     });
   });
 
-  DOMElementsMap.openDocumentButton.addEventListener("click", () => {
-    ipcRenderer.send("open-document-triggered");
+  DOMElementsMap.openDocumentButton.addEventListener('click', () => {
+    ipcRenderer.send('open-document-triggered');
   });
 
-  DOMElementsMap.createDocumentButton.addEventListener("click", () => {
-    ipcRenderer.send("new-document-triggered");
-    DOMElementsMap.textareaInput.value = "";
+  DOMElementsMap.createDocumentButton.addEventListener('click', () => {
+    ipcRenderer.send('new-document-triggered');
+    DOMElementsMap.textareaInput.value = '';
   });
 
-  DOMElementsMap.closeApplicationButton.addEventListener("click", () => {
-    ipcRenderer.send("close-application-triggered");
+  DOMElementsMap.closeApplicationButton.addEventListener('click', () => {
+    ipcRenderer.send('close-application-triggered');
   });
 
-  DOMElementsMap.textareaInput.addEventListener("input", () => {
-    DOMElementsMap.documentNameStar.innerText = "*";
+  DOMElementsMap.textareaInput.addEventListener('input', () => {
+    DOMElementsMap.documentNameStar.innerText = '*';
   });
 
   // Renderer event listeners
@@ -44,12 +44,12 @@ window.addEventListener("DOMContentLoaded", () => {
 
   ipcRenderer.on(EMainCustomEvents.DOCUMENT_SAVED, (_, args) => {
     DOMElementsMap.documentName.innerText = args.title;
-    DOMElementsMap.documentNameStar.innerText = "";
+    DOMElementsMap.documentNameStar.innerText = '';
   });
 
   ipcRenderer.on(EMainCustomEvents.NEW_DOCUMENT_CREATED, () => {
-    DOMElementsMap.documentName.innerText = "";
-    DOMElementsMap.documentNameStar.innerText = "";
+    DOMElementsMap.documentName.innerText = '';
+    DOMElementsMap.documentNameStar.innerText = '';
   });
 
   DOMElementsMap.textareaInput.focus();
--- src/renderer.ts
+++ src/renderer.ts
@@ -1,4 +1,4 @@
-import "./index.scss";
+import './index.scss';
 
 console.log(
   '👋 This message is being logged by "renderer.js", included via webpack'
