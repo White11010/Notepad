7a5421639777f73c8fc7dfe2f5f1d2684d9bab87
White11010 <beliavski26@gmail.com>
2022-10-13 12:29:52 +0300

    renamed typings folder

---
+++ src/models/custom-events.ts
@@ -0,0 +1,12 @@
+export enum ERendererCustomEvents {
+	SAVE_DOCUMENT = 'save-document-triggered',
+	OPEN_DOCUMENT = 'open-document-triggered',
+	CREATE_NEW_DOCUMENT = 'new-document-triggered',
+	CLOSE_APPLICATION = 'close-application-triggered',
+}
+
+export enum EMainCustomEvents {
+	DOCUMENT_OPENED = 'document-opened',
+	DOCUMENT_SAVED = 'document-saved',
+	NEW_DOCUMENT_CREATED = 'new-document-created'
+}
