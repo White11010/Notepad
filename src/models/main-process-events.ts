3e393efd359c219aeb725bb1cd4af96d4b451f34
White11010 <beliavski26@gmail.com>
2022-10-13 13:03:36 +0300

    add interfaces for events payload

---
+++ src/models/main-process-events.ts
@@ -0,0 +1,8 @@
+export interface IDocumentSavedEventPayload {
+	title: string,
+}
+
+export interface IDocumentOpenedEventPayload {
+	text: string,
+	title: string,
+}
--- src/preload.ts
+++ src/preload.ts
@@ -1,56 +1,61 @@
-import { ipcRenderer } from 'electron';
-import {EMainCustomEvents} from './models/custom-events';
+import {ipcRenderer} from 'electron';
+import {EMainCustomEvents, ERendererCustomEvents} from './models/custom-events-enums';
+import {ISaveDocumentEventPayload} from './models/renderer-process-events';
+import {IDocumentOpenedEventPayload, IDocumentSavedEventPayload} from './models/main-process-events';
 
 window.addEventListener('DOMContentLoaded', () => {
-  const DOMElementsMap = {
-    saveDocumentButton: document.getElementById('save-button'),
-    createDocumentButton: document.getElementById('new-button'),
-    openDocumentButton: document.getElementById('open-button'),
-    closeApplicationButton: document.getElementById('close-button'),
-    textareaInput: document.getElementById('textarea-input') as HTMLInputElement,
-    documentName: document.getElementById('document-name'),
-    documentNameStar: document.getElementById('document-name-star'),
-  };
+	const DOMElementsMap = {
+		saveDocumentButton: document.getElementById('save-button'),
+		createDocumentButton: document.getElementById('new-button'),
+		openDocumentButton: document.getElementById('open-button'),
+		closeApplicationButton: document.getElementById('close-button'),
+		textareaInput: document.getElementById('textarea-input') as HTMLInputElement,
+		documentName: document.getElementById('document-name'),
+		documentNameStar: document.getElementById('document-name-star'),
+	};
 
-  // DOM elements event listeners
-  DOMElementsMap.saveDocumentButton.addEventListener('click', () => {
-    ipcRenderer.send('save-document-triggered', {
-      inputText: DOMElementsMap.textareaInput.value,
-    });
-  });
+	// DOM elements event listeners
+	DOMElementsMap.saveDocumentButton.addEventListener('click', () => {
+		ipcRenderer.send(
+			ERendererCustomEvents.SAVE_DOCUMENT,
+			{
+				inputText: DOMElementsMap.textareaInput.value,
+			} as ISaveDocumentEventPayload
+		);
+	});
 
-  DOMElementsMap.openDocumentButton.addEventListener('click', () => {
-    ipcRenderer.send('open-document-triggered');
-  });
+	DOMElementsMap.openDocumentButton.addEventListener('click', () => {
+		ipcRenderer.send(ERendererCustomEvents.OPEN_DOCUMENT);
+	});
 
-  DOMElementsMap.createDocumentButton.addEventListener('click', () => {
-    ipcRenderer.send('new-document-triggered');
-    DOMElementsMap.textareaInput.value = '';
-  });
+	DOMElementsMap.createDocumentButton.addEventListener('click', () => {
+		ipcRenderer.send(ERendererCustomEvents.CREATE_NEW_DOCUMENT);
+		DOMElementsMap.textareaInput.value = '';
+	});
 
-  DOMElementsMap.closeApplicationButton.addEventListener('click', () => {
-    ipcRenderer.send('close-application-triggered');
-  });
+	DOMElementsMap.closeApplicationButton.addEventListener('click', () => {
+		ipcRenderer.send(ERendererCustomEvents.CLOSE_APPLICATION);
+	});
 
-  DOMElementsMap.textareaInput.addEventListener('input', () => {
-    DOMElementsMap.documentNameStar.innerText = '*';
-  });
+	DOMElementsMap.textareaInput.addEventListener('input', () => {
+		DOMElementsMap.documentNameStar.innerText = '*';
+	});
 
-  // Renderer event listeners
-  ipcRenderer.on(EMainCustomEvents.DOCUMENT_OPENED, (_, args) => {
-    DOMElementsMap.textareaInput.value = args.text;
-    DOMElementsMap.documentName.innerText = args.title;
-  });
+	// Renderer event listeners
+	ipcRenderer.on(EMainCustomEvents.DOCUMENT_OPENED, (_, args: IDocumentOpenedEventPayload) => {
+		DOMElementsMap.textareaInput.value = args.text;
+		DOMElementsMap.documentName.innerText = args.title;
+	});
 
-  ipcRenderer.on(EMainCustomEvents.DOCUMENT_SAVED, (_, args) => {
-    DOMElementsMap.documentName.innerText = args.title;
-    DOMElementsMap.documentNameStar.innerText = '';
-  });
+	ipcRenderer.on(EMainCustomEvents.DOCUMENT_SAVED, (_, args: IDocumentSavedEventPayload) => {
+		DOMElementsMap.documentName.innerText = args.title;
+		DOMElementsMap.documentNameStar.innerText = '';
+	});
 
-  ipcRenderer.on(EMainCustomEvents.NEW_DOCUMENT_CREATED, () => {
-    DOMElementsMap.documentName.innerText = '';
-    DOMElementsMap.documentNameStar.innerText = '';
-  });
+	ipcRenderer.on(EMainCustomEvents.NEW_DOCUMENT_CREATED, () => {
+		DOMElementsMap.documentName.innerText = '';
+		DOMElementsMap.documentNameStar.innerText = '';
+	});
 
-  DOMElementsMap.textareaInput.focus();
+	DOMElementsMap.textareaInput.focus();
 });
